d3a31927f23e839e29b925c88670be72af4a164e
jdubrey <132595743+jdubrey@users.noreply.github.com>
2023-05-11 19:19:46 -0400

    JS added, apiKey, apiUrl

--- src/index.js
+++ src/index.js
@@ -15,3 +15,41 @@ let currentMinute = now.getMinutes();
 let today = `${currentDay}, ${currentTime}:${currentMinute},`;
 let day = document.querySelector("#currentDay");
 day.innerHTML = today;
+
+function search(event) {
+  event.preventDefault();
+  let update = document.querySelector("#city");
+  let pinpoint = document.querySelector("#cityWeather");
+  pinpoint.innerHTML = update.value;
+  searchCity(update.value);
+}
+
+function newForecast(response) {
+  console.log(response);
+  console.log(response.data.main.temp);
+  console.log(response.data.main.humidity);
+  console.log(response.data.weather[0].description);
+  let newTemp = document.querySelector("#today-temp");
+  newTemp.innerHTML = `${Math.round(response.data.main.temp)}`;
+
+  let newHumidity = document.querySelector("#humidity");
+  newHumidity.innerHTML = `Humidity: ${Math.round(
+    response.data.main.humidity
+  )}%`;
+
+  let newDescrption = document.querySelector("#weather-description");
+  newDescrption.innerHTML = response.data.weather[0].description;
+
+  let newCity = document.querySelector("#cityWeather");
+  newCity.innerHTML = response.data.name;
+}
+
+function searchCity(city) {
+  let apiKey = "odc80af43206eetf0e915ae198eb039c";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=odc80af43206eetf0e915ae198eb039c&units=metric`;
+  axios.get(apiUrl).then(newForecast);
+}
+searchCity("Miami");
+
+let exact = document.querySelector("#button");
+exact.addEventListener("click", searchCity);
