dba041956a37cc768a4d96c710cdd1003de4b7b0
jdubrey <132595743+jdubrey@users.noreply.github.com>
2023-05-16 18:09:05 -0400

    update JS to display weekly forecast

--- src/index.js
+++ src/index.js
@@ -16,31 +16,47 @@ let today = `${currentDay}, ${currentTime}:${currentMinute},`;
 let day = document.querySelector("#currentDay");
 day.innerHTML = today;
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+
 function displayForecast(response) {
   console.log(response.data.daily);
   let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
 
-  forecast.forEach(function (forecastDay) {
-    forecastHTML =
-      forecastHTML +
-      `
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
     <div class="col-2">
-      ${forecastDay.time}
+      ${formatDay(forecastDay.time)}
+   
       <img
         class="weekly-img"
-        src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${forecastDay.condition.icon}.png"
+        src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${
+          forecastDay.condition.icon
+        }.png"
         alt="broken clouds"
         width="48"
         height="48"
       />
       <div class="forecast-temperature">
-        <span class="forecast-temperature-max">${forecastDay.temperature.maximum}° </span>
-        <span class="forecast-temperature-min">${forecastDay.temperature.minimum}°</span>
+        <span class="forecast-temperature-max">${Math.round(
+          forecastDay.temperature.maximum
+        )}° </span>
+        <span class="forecast-temperature-min">${Math.round(
+          forecastDay.temperature.minimum
+        )}°</span>
       </div>
     </div>
   `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
